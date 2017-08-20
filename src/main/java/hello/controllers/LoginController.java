4e69541763f1b9eba2d4032b090b8737a4b7cc72
Sahej Kaur <sahejk@thoughtworks.com>
2017-08-20 13:33:26 +0530

    Correcting categories endpoint

--- src/main/java/hello/controllers/LoginController.java
+++ src/main/java/hello/controllers/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
-  @RequestMapping(value = "/v1/cc-reward-redemption/rewards", method = RequestMethod.GET)
+  @RequestMapping(value = "/v1/cc-reward-redemption/categories", method = RequestMethod.GET)
   public ResponseEntity<String> categories() {
     return new ResponseEntity<>("{\n" +
       "    \"status\": {\n" +
